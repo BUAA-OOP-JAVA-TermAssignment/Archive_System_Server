9df4bd236fd5fe2b0490c81642fbda69e8b24cf1
pcpas <1134380859@qq.com>
2022-12-11 18:26:34 +0800

    修改了部分msg使其可以序列化

--- src/message/AdminUserRequestMsg.java
+++ src/message/AdminUserRequestMsg.java
@@ -1,25 +1,28 @@
 package message;
 
+import data.AdminData;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
 
-public class AdminUserRequestMsg extends BaseMsg{
+public class AdminUserRequestMsg extends BaseMsg {
 
     private ArrayList<User> userArrayList;
 
 
-    public class User {
-        private String userName;
-        private String id;
-        private String password;
-        private int downloadCnt;
-        private String date;
-        private String email;
+    public class User implements Serializable {
+        String userName;
+        String id;
+        String password;
+        int downloadCnt;
+        String date;
+        String email;
 
         /**
          * Òª×¢ÊÍµô£¡
          */
-        User(String userName, String id, String password, int downloadCnt, String date,String email){
+        User(String userName, String id, String password, int downloadCnt, String date, String email) {
             this.userName = userName;
             this.id = id;
             this.password = password;
@@ -34,41 +37,40 @@ public class AdminUserRequestMsg extends BaseMsg{
         this.userArrayList = new ArrayList<>();
     }
 
-    public void add(String id, String userName, String password, String email, int downloadCnt, String date){
-        userArrayList.add(new User(userName,id,password,downloadCnt,date,email));
-    }
 
-    public int getUserNum(){
-        if(userArrayList == null){
+    public int getUserNum() {
+        if (userArrayList == null) {
             return 0;
         }
         return userArrayList.size();
     }
-    public User getUserInfo(int i){
+
+    public AdminUserRequestMsg.User getUserInfo(int i) {
         return userArrayList.get(i);
     }
 
-    public String getUserName(int idx){
+    public String getUserName(int idx) {
         return userArrayList.get(idx).userName;
     }
 
-    public String getUserId(int idx){
+    public String getUserId(int idx) {
         return userArrayList.get(idx).id;
     }
 
-    public String getUserEmail(int idx){
+    public String getUserEmail(int idx) {
         return userArrayList.get(idx).email;
     }
 
-    public String getUserPassword(int idx){
+    public String getUserPassword(int idx) {
         return userArrayList.get(idx).password;
     }
 
-    public int getUserDownloadCnt(int idx){
+    public int getUserDownloadCnt(int idx) {
         return userArrayList.get(idx).downloadCnt;
     }
 
-    public String getUserDate(int idx){
+    public String getUserDate(int idx) {
         return userArrayList.get(idx).date;
     }
+
 }
